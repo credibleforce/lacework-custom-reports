6587b3b7d27ea6091585becd0beb11374ea3a233
credibleforce <jamiefrasermcmurray@gmail.com>
2021-11-30 17:16:44 -0800

    Update templates and add file handler

--- jef/report/report_handler.py
+++ jef/report/report_handler.py
@@ -13,6 +13,9 @@ class report_handler():
         self.settings = settings
         self.report = report
         self.template = None
+        self.attachment_template = None
+        self.attachment_name = None
+        self.attachment_comment = None
         self.parse_template()
 
     def parse_template(self):
@@ -25,6 +28,16 @@ class report_handler():
             loader = jinja2.FileSystemLoader(searchpath=os.path.dirname(self.settings['template']))
             env = jinja2.Environment(loader=loader,extensions=['jinja2.ext.do'])
             self.template = env.get_template(os.path.basename(self.settings['template']))
+
+        # allow for attachment formatting when set report handler will be used to 'attach' result in that context
+        if 'attachment_template' in self.report.keys():
+            loader = jinja2.FileSystemLoader(searchpath=os.path.dirname(self.report['attachment_template']))
+            env = jinja2.Environment(loader=loader,extensions=['jinja2.ext.do'])
+            self.attachment_template = env.get_template(os.path.basename(self.report['attachment_template']))
+            
+            # set default name and comment values
+            self.attachment_name = self.report['attachment_name'] if 'attachment_name' in self.report.keys() else 'report'
+            self.attachment_comment = self.report['attachment_comment'] if 'attachment_comment' in self.report.keys() else 'report'
 
     def generate(self):
         pass
--- jef/report/slack_report_handler.py
+++ jef/report/slack_report_handler.py
@@ -1,3 +1,4 @@
+from ssl import CHANNEL_BINDING_TYPES
 from slack.web.slack_response import SlackResponse
 from .report_handler import report_handler
 from slack import WebClient
@@ -10,6 +11,11 @@ module_path = os.path.abspath(os.path.dirname(__file__))
 class slack_report_handler(report_handler):
     def generate(self):
         # as we can't submit json directory with the client we parse first and then supply args
+        attachment_from_template = None
+        if self.attachment_template is not None:
+            self.logger.info("Attachment template defined - parsing...")
+            attachment_from_template = self.attachment_template.render(items=self.datasets)
+
         message_from_template = self.template.render(items=self.datasets,channel=self.report['channel'])
         slack_message = json.loads(message_from_template)
         client = WebClient(token=self.report['token'])
@@ -21,6 +27,21 @@ class slack_report_handler(report_handler):
                 attachments=(slack_message['attachments'] if 'attachments' in slack_message.keys() is not None else None),
                 blocks=(slack_message['blocks'] if 'blocks' in slack_message.keys() is not None else None)
             )
+            
+            # if we have a templated attachment upload content to same thread
+            if self.attachment_template is not None:
+                try:
+                    upload_reponse = client.files_upload(
+                        channels=self.report['channel'],
+                        content=attachment_from_template,
+                        filename=self.report['attachment_name'],
+                        initial_comment=self.report['attachment_comment'],
+                        thread_ts=response['ts']
+                    )
+                except SlackApiError as e:
+                    self.logger.error(e.response["error"]) # str like 'invalid_auth', 'channel_not_found'
+                
         except SlackApiError as e:
+            self.logger.error(e.response["error"])
             # You will get a SlackApiError if "ok" is False
             assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
