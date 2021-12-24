01fe07bf13ea53b7d1dae4281603939f730fee2f
credibleforce <jamiefrasermcmurray@gmail.com>
2021-12-23 17:49:44 -0800

    Update base

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM python:3.10-slim
 
-RUN groupadd --gid 5000 user && useradd --home-dir /home/user --create-home --uid 5000 --gid 5000 --shell /bin/sh --skel /dev/null user
+RUN groupadd --gid 5000 user && useradd --home-dir /home/user --create-home --uid 5000 --gid 5000 --shell /bin/sh --skel /dev/null user && chmod 755 /home/user
 RUN apt-get update && apt-get install -y curl
-RUN python -m pip install --upgrade pip
 RUN curl https://raw.githubusercontent.com/lacework/go-sdk/main/cli/install.sh | bash
+
+RUN python -m pip install --upgrade pip
+COPY requirements.txt ./
+RUN pip install -r requirements.txt
 
 USER user
 
@@ -12,9 +15,6 @@ ENV LANG=C.UTF-8
 ENV PATH "$PATH:/home/user/.local/bin"
 
 WORKDIR /app
-
-COPY requirements.txt ./
-RUN pip install -r requirements.txt
 
 COPY reports.py ./
 COPY jef ./jef
--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-docker run --rm \
+docker run --rm -it \
+    `# name of the container` \
+    --name custom-reporting \
+    `# mount credentials and configuration` \
     -v ~/.lacework.toml:/home/user/.lacework.toml \
     -v $(pwd)/output:/app/output \
     -v $(pwd)/reports:/app/reports \
     -v $(pwd)/templates:/app/templates \
+    `# report environment variables` \
     --env=GCP_PROJECT=kubernetes-cluster-331006 \
     --env=GCP_ORG=286188307222 \
-    --name custom-reporting \
-    credibleforce/custom-reporting --config /app/reports/laceworkcli/laceworkcli_gcp_compliance_html_report_docker.json
+    `# override the uid to allow docker write through to output volume (optional)` \
+    --env=HOME=/home/user \
+    --user $UID:$GID \
+    `# run the report script` \
+    credibleforce/custom-reporting --config /app/reports/laceworkcli/laceworkcli_gcp_compliance_html_report.json
