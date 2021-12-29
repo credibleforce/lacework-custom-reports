fcbfcce9d690a7909e69d2294ebfdb82ad9f2812
credibleforce <jamiefrasermcmurray@gmail.com>
2021-12-28 20:10:43 -0800

    Update base

--- test_lacework_custom_reports.py
+++ test_lacework_custom_reports.py
@@ -6,33 +6,11 @@ from src.lacework_custom_reports import __main__ as main
 
 
 def test_main_default():
-    args = argparse.Namespace(account=None,
-                              subaccount=None,
-                              api_key=None,
-                              api_secret=None,
-                              profile=None,
-                              days=None,
-                              hours=0,
-                              registry=None,
-                              rescan=None,
-                              list_only=None,
-                              daemon=None,
-                              debug=None)
+    args = argparse.Namespace(config=None)
     main.run(args)
 
 
 def test_main_config_fail():
-    args = argparse.Namespace(account='testing',
-                              subaccount=None,
-                              api_key=None,
-                              api_secret=None,
-                              profile=None,
-                              days=None,
-                              hours=0,
-                              registry=None,
-                              rescan=None,
-                              list_only=None,
-                              daemon=None,
-                              debug=None)
+    args = argparse.Namespace()
     with pytest.raises(Exception):
         main.run(args)
